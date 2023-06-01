45939e558528bc7679e92707b29e3c7ede58270a
El-Praise Ayo <elpraise20@gmail.com>
2023-06-01 10:36:19 +0100

    Experiece change edits

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,18 +30,18 @@ function Hero() {
               beautiful and user-friendly websites. <br /> Check out my work and
               let's collaborate!
             </p>
-            <motion.button
-              animate={{
-                x: [-10, 10, -10, 10, 0],
-                transition: { delay: 4, duration: 2 },
-              }}
-              layout
-              className="btn mt-5 w-full h-1 lg:w-48 md:w-96 sm:w-96 hover:btn-secondary"
-            >
-              <a href="https://drive.google.com/file/d/1cU0vTt7KkxBbMyNl4-7LXYkRp2ljUcAu/view?usp=sharing">
+            <a href="https://drive.google.com/file/d/1cU0vTt7KkxBbMyNl4-7LXYkRp2ljUcAu/view?usp=sharing">
+              <motion.button
+                animate={{
+                  x: [-10, 10, -10, 10, 0],
+                  transition: { delay: 4, duration: 2 },
+                }}
+                layout
+                className="btn mt-5 w-full h-1 lg:w-48 md:w-96 sm:w-96 hover:btn-secondary"
+              >
                 Resume
-              </a>
-            </motion.button>
+              </motion.button>
+            </a>
           </div>
           <motion.img
             animate={{ x: [0, -5, 5, -5, 5, 0] }}
